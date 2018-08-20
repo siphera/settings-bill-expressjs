19b69aaab0d4aea87922824a3ee53a0e23812b33
siphera <salmansiphenkosi@gmail.com>
2018-08-20 14:44:30 +0200

    mmm

--- test/settings-bill.test.js
+++ test/settings-bill.test.js
@@ -3,78 +3,78 @@ let SettingsBill = require("../settings-bill");
 
 describe('Settings widget using ExpressJS', function () {
     it('should calculate total cost of phone calls made', function () {
-        let Set = SettingsBill();
+        let setting = SettingsBill();
 
-        Set.callPrice(2);
-        Set.bill_Type('call');
+        setting.callPrice(2);
+        setting.bill_Type('call');
 
-        Set.bill_Type('call');
-        Set.bill_Type('call');
-        Set.bill_Type('call');
+        setting.bill_Type('call');
+        setting.bill_Type('call');
+        setting.bill_Type('call');
 
-        assert.equal(Set.totalCost(), 8);
+        assert.equal(setting.totalCost(), 8);
 
     });
 
     it('should calculate total cost of smses sent', function () {
-        let Set = SettingsBill()
+        let setting = SettingsBill()
 
-        Set.smsPrice(0.75);
-        Set.bill_Type('sms');
-        Set.bill_Type('sms');
+        setting.smsPrice(0.75);
+        setting.bill_Type('sms');
+        setting.bill_Type('sms');
 
-        assert.equal(Set.totalCost(), 1.5);
+        assert.equal(setting.totalCost(), 1.5);
     });
 
     it('should calculate total cost of smses sent and calls made', function () {
-        let Set = SettingsBill()
+        let setting = SettingsBill()
 
-        Set.smsPrice(0.75);
-        Set.bill_Type('sms');
-        Set.bill_Type('sms');
-        Set.callPrice(2);
-        Set.bill_Type('call');
-        Set.bill_Type('call');
-        Set.bill_Type('call');
-        Set.bill_Type('call');
+        setting.smsPrice(0.75);
+        setting.bill_Type('sms');
+        setting.bill_Type('sms');
+        setting.callPrice(2);
+        setting.bill_Type('call');
+        setting.bill_Type('call');
+        setting.bill_Type('call');
+        setting.bill_Type('call');
 
-        assert.equal(Set.totalCost(), 9.5);
+        assert.equal(setting.totalCost(), 9.5);
     });
 
     it('should return given amount that is charged for an sms', function () {
-        let Set = SettingsBill()
+        let setting = SettingsBill()
 
-        Set.smsPrice(0.75);
-        Set.bill_Type('sms')
+        setting.smsPrice(0.75);
+        setting.bill_Type('sms')
 
-        assert.equal(Set.smsTotal(), 0.75);
+        assert.equal(setting.smsTotal(), 0.75);
     });
 
     it('should return given amount that is charged for a call', function () {
-        let Set = SettingsBill()
+        let setting = SettingsBill()
 
-        Set.callPrice(2.75);
-        Set.bill_Type('call')
+        setting.callPrice(2.75);
+        setting.bill_Type('call')
 
-        assert.equal(Set.callTotal(), 2.75);
+        assert.equal(setting.callTotal(), 2.75);
     });
 
 
 
 
     it('should return that warning level is reached', function () {
-        let Set = SettingsBill()
+        let setting = SettingsBill()
 
-        Set.warningL(10);
+        setting.warningL(10);
 
-        assert.equal(Set.getWarningLevel(), 10);
+        assert.equal(setting.getWarningLevel(), 10);
     });
 
 
     it('should return that critical level is reached', function () {
-        let Set = SettingsBill()
-        Set.criticalL(10);
-        assert.equal(Set.getCriticalLevel(), 10);
+        let setting = SettingsBill()
+        setting.criticalL(10);
+        assert.equal(setting.getCriticalLevel(), 10);
     });
 
 
